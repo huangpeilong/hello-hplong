460198d2f0b12063c1f91cfc9344ff5a9456258a
vagrant <vagrant@localhost.localdomain>
2017-06-26 08:33:16 +0000

    modify patch

--- config_diff.sh
+++ config_diff.sh
@@ -2,6 +2,6 @@
 
 # make diff file for /etc/keepalived/keepalived.conf
 # diff -uNr keepalived keepalived_dest > etc_keepalived.patch
-diff -uN keepalived/keepalived.conf keepalived_dest/keepalived.conf > etc_keepalived.patch
+diff -uN keepalived_init/keepalived.conf keepalived_dest/keepalived.conf > etc_keepalived.patch
 
 # make diff file for /home/nginx/conf
